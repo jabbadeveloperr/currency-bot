5682f69d0d8dec4764bc27996c2bbc12237d5988
Vitalii Lytvynov <59739827+jabbadeveloperr@users.noreply.github.com>
2023-10-11 18:18:51 +0300

    remove comments

--- src/main/java/bot/currency/scheduler/CurrencyPriceScheduler.java
+++ src/main/java/bot/currency/scheduler/CurrencyPriceScheduler.java
@@ -19,16 +19,12 @@ public class CurrencyPriceScheduler {
         this.currencyChangeQueue = currencyChangeQueue;
     }
 
-
     public void startUpdates() {
         AtomicReference<String> json = new AtomicReference<>("");
 
         ScheduledExecutorService scheduler = Executors.newSingleThreadScheduledExecutor();
 
         scheduler.scheduleAtFixedRate(() -> {
-            // Виконуємо запит на отримання змін валют
-
-
             String result = getSourceJson();
             if (json.get().isEmpty()) {
                 json.set(result);
@@ -36,45 +32,15 @@ public class CurrencyPriceScheduler {
 
             Map<String, String> currencyChanges =
                     new PriceDifferentService(result, json.get()).getChangedCurrencies();
-            // Додаємо отримані зміни в чергу
             for (Map.Entry<String, String> entry : currencyChanges.entrySet()) {
                 currencyChangeQueue.addCurrencyChange(entry.getKey(), entry.getValue());
             }
-        }, 0, 10, TimeUnit.SECONDS); // Перший параметр - відразу, другий - період
+        }, 0, 10, TimeUnit.SECONDS); 
     }
-//    private static void scheduleBySecond(int seconds) {
-//        ScheduledExecutorService scheduler = Executors.newSingleThreadScheduledExecutor();
-//        AtomicReference<String> json = new AtomicReference<>("");
-//
-//        scheduler.scheduleAtFixedRate(() -> {
-//            String result = getSourceJson();
-//            if (json.get().isEmpty()) {
-//                json.set(result);
-//            }
-////telegram magic start
-//            Map<String, String> changedCurrencies =
-//                    new PriceDifferentService(result, json.get()).getChangedCurrencies();
-//             changedCurrencies.forEach((currency, price) -> {
-//                System.out.println("Currency: " + currency + ", Price: " + price + "caunt" );
-//            });
-//            json.set(result);
-////telegram magic end
-//
-//
-//        }, 0, seconds, TimeUnit.SECONDS); // Перший параметр - відразу, другий - період
-//    }
 
     private static String getSourceJson() {
         RestTemplate restTemplate = new RestTemplate();
         String result = restTemplate.getForObject(URL_TO_CHECK_PRICES, String.class);
         return result;
     }
-
-
-
-    // Метод для отримання JSON та його обробки (парсингу)
-
-
-    // Колбек для обробки отриманих змін валют
-
 }
